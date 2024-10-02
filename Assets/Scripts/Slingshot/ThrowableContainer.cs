ce160ecbf6cf81bae3fec23798507ec58d0bd0d7
Dmytro Buriak <buriak.dmytro1121@vu.cdu.edu.ua>
2024-10-02 23:32:03 +0300

    fix(birds container): move excessive logic to other related scripts

--- Assets/Scripts/Slingshot/ThrowableContainer.cs
+++ Assets/Scripts/Slingshot/ThrowableContainer.cs
@@ -10,13 +10,6 @@ namespace Slingshot
         [HideInInspector]
         public GameObject CurrentThrowable = null;
         
-        public Transform ShotPointActual;
-        public Transform ShotPointReference;
-
-        [SerializeField]
-        [Range(30.0f, 100.0f)]
-        private float _tensionForcePerDistanceUnit;
-
         void Start()
         {
             SetupNewThrowableToShoot();
@@ -35,67 +28,12 @@ namespace Slingshot
             }
         }
 
-        public void ReleaseCurrentThrowable()
-        {
-            Rigidbody rigidbody = SetUpPhysicsForThrowable();
-
-            float stretchDistance = CalculateDistanceToShotReference();
-            Vector3 forceDirection = CalculateAppliedForceDirection();
-
-            FreeThrowable();
-            ApplyImpulseToThrowable(rigidbody, forceDirection, stretchDistance);
-            ForgetThrowable();
-        }
-
-        private Rigidbody SetUpPhysicsForThrowable()
-        {
-            CurrentThrowable.AddComponent<SphereCollider>();
-
-            Rigidbody rb = CurrentThrowable.AddComponent<Rigidbody>();
-            rb.mass = 6;
-            rb.angularDrag = 3;
-
-            return rb;
-        }
-
-        private float CalculateDistanceToShotReference()
-        {
-            float distance = 
-                Vector3.Distance(
-                    ShotPointReference.position,
-                    ShotPointActual.position
-                );
-
-            Debug.Log("Distance: " + distance);
-
-            return distance;
-        }
-
-        private Vector3 CalculateAppliedForceDirection()
-        {
-            Vector3 direction =
-                (ShotPointReference.position - ShotPointActual.position)
-                .normalized;
-
-            return direction;
-        }
-
-        private void FreeThrowable()
+        public void FreeThrowable()
         {
             CurrentThrowable.transform.parent = null;
         }
 
-        private void ApplyImpulseToThrowable(
-            Rigidbody rigidbody,
-            Vector3 forceDirection,
-            float stretchDistance)
-        {
-            rigidbody.AddForce(
-                _tensionForcePerDistanceUnit * stretchDistance *
-                forceDirection);
-        }
-
-        private void ForgetThrowable()
+        public void ForgetThrowable()
         {
             CurrentThrowable = null;
         }
