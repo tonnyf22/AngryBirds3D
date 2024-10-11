7ab6648a6b016aff1aa406b8f3fda5f2ef52f312
Dmytro Buriak <buriak.dmytro1121@vu.cdu.edu.ua>
2024-10-11 23:47:39 +0300

    feat(slingshot input): completely rewrite input detections

--- Assets/Scripts/Slingshot/SlingshotSpringInput.cs
+++ Assets/Scripts/Slingshot/SlingshotSpringInput.cs
@@ -1,92 +1,170 @@
 using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 using TouchPhase = UnityEngine.InputSystem.TouchPhase;
 
 namespace AngryBirds3D.Slingshot
 {
-    public class SlingshotSpringInput : MonoBehaviour
-    {
-        public event Action<Vector3> ChangeThrowablePositionAndForwardDirectionEvent;
-        public event Action RecalculateTrajectoryPredictionEvent;
+	public class SlingshotSpringInput : MonoBehaviour
+	{
+		public event Action<Vector3> ChangeThrowablePositionAndForwardDirectionEvent;
+		public event Action RecalculateTrajectoryPredictionEvent;
 
-        public event Action InitiateReleaseLogicEvent;
+		public event Action<float> RotateSlingshotEvent;
 
-        [SerializeField]
-        private InputAction _touchInputAction;
+		public event Action InitiateReleaseLogicEvent;
 
-        [SerializeField]
-        private LayerMask _touchPlaneLayer;
+		[SerializeField]
+		private LayerMask _touchPlaneLayer;
 
-        private Camera _camera;
+		[SerializeField]
+		[Range(1.0f, 20.0f)]
+		private float _horizontalAimSensitivity;
 
-        // raycast related
-        private Vector2 _touchPosition;
-        private Ray _ray;
-        private RaycastHit _hit;
+		private Camera _camera;
+		
+		// raycast related
+		private Ray _ray;
+		private RaycastHit _hit;
 
-        void OnEnable()
-        {
-            _touchInputAction.Enable();
-        }
+		private Vector3 _lastHitPoint;
 
-        void OnDisable()
-        {
-            _touchInputAction.Disable();
-        }
+		void Start()
+		{
+			_camera = Camera.main;
+		}
 
-        void Start()
-        {
-            _camera = Camera.main;
-        }
+		private bool _isTensionTouchTracked;
+		private Touch _tensionTouch;
 
-        void Update()
-        {
-            ServeTouchInput();
-        }
+		private bool _isAimTouchTracked;
+		private Touch _aimTouch;
 
-        private void ServeTouchInput()
-        {
-            if (_touchInputAction.triggered)
-            {
-                // remember the touch on tension plane when it starts and wait 'till it ends
-                RaycastFromTouchPosition();
-            }
+		void OnEnable()
+		{
+			_aimTouch = new Touch();
+			_tensionTouch = new Touch();
+		}
 
-            CheckInputRelease();
-        }
+		void OnDisable()
+		{
+		}
 
-        private void RaycastFromTouchPosition()
-        {
-            _touchPosition = _touchInputAction.ReadValue<Vector2>();
+		void Update()
+		{
+			ServeTensionInput();
+			ServeAimInput();
+		}
 
-            if (IsHitTouchPlane())
-            {
-                ChangeThrowablePositionAndForwardDirectionEvent?.Invoke(_hit.point);
+		private void ServeTensionInput()
+		{
+			if (Touch.activeTouches.Count > 0)
+			{
+				if (!_isTensionTouchTracked)
+				{
+					if (IsHitTensionPlane(Touch.activeTouches[0].screenPosition))
+					{
+						_isTensionTouchTracked = true;
+						_tensionTouch = Touch.activeTouches[0];
+					}
+					else if (Touch.activeTouches.Count > 1)
+					{
+						if (IsHitTensionPlane(Touch.activeTouches[1].screenPosition))
+						{
+							_isTensionTouchTracked = true;
+							_tensionTouch = Touch.activeTouches[1];
+						}
+					}
+				}
+				else
+				{
+					if (_tensionTouch.phase == TouchPhase.Moved || _tensionTouch.phase == TouchPhase.Stationary)
+					{
+						// thats a prediction obviously
 
-                RecalculateTrajectoryPredictionEvent?.Invoke();
-            }
-        }
+						if (IsHitTensionPlane(_tensionTouch.screenPosition))
+						{
+							_lastHitPoint = _hit.point;
+						}
 
-        private bool IsHitTouchPlane()
-        {
-            _ray = _camera.ScreenPointToRay(_touchPosition);
+						ChangeThrowablePositionAndForwardDirectionEvent?.Invoke(_lastHitPoint);
 
-            return Physics.Raycast(_ray, out _hit, Mathf.Infinity, _touchPlaneLayer);
-        }
+						RecalculateTrajectoryPredictionEvent?.Invoke();
+					}
+					else if (_tensionTouch.phase == TouchPhase.Ended || _tensionTouch.phase == TouchPhase.Canceled)
+					{
+						// thats a release obviously
 
-        private void CheckInputRelease()
-        {
-            if (Touch.activeTouches.Count > 0)
-            {
-                Touch touch = Touch.activeTouches[0];
+						_isTensionTouchTracked = false;
 
-                if (touch.phase == TouchPhase.Ended)
-                {
-                    InitiateReleaseLogicEvent?.Invoke();
-                }
-            }
-        }
-    }
+						InitiateReleaseLogicEvent?.Invoke();
+					}
+				}
+			}
+		}
+
+		private bool IsHitTensionPlane(Vector2 screenTouchPosition)
+		{
+			_ray = 
+				_camera.ScreenPointToRay(screenTouchPosition);
+
+			return 
+				Physics.Raycast(
+					_ray, 
+					out _hit, 
+					Mathf.Infinity, 
+					_touchPlaneLayer);
+		}
+
+		private void ServeAimInput()
+		{
+			if (Touch.activeTouches.Count > 0)
+			{
+				if (!_isAimTouchTracked)
+				{
+					if (_tensionTouch.valid)
+					{
+						if (Touch.activeTouches[0].touchId != _tensionTouch.touchId)
+						{
+							_isAimTouchTracked = true;
+							_aimTouch = Touch.activeTouches[0];
+						}
+						else if (Touch.activeTouches.Count > 1)
+						{
+							if (Touch.activeTouches[1].touchId != _tensionTouch.touchId)
+							{
+								_isAimTouchTracked = true;
+								_aimTouch = Touch.activeTouches[1];
+							}
+						}
+					}
+				}
+				else
+				{
+					if (_aimTouch.phase == TouchPhase.Moved || _aimTouch.phase == TouchPhase.Stationary)
+					{
+						Vector2 dragRaw = _aimTouch.delta;
+						Vector2 drag = AdjustAimDrag(dragRaw);
+						float rotation = CalculateHorisontalRotationFromAimDrag(drag);
+
+						RotateSlingshotEvent?.Invoke(rotation);
+					}
+					else if (_aimTouch.phase == TouchPhase.Ended || _aimTouch.phase == TouchPhase.Canceled)
+					{
+						_isAimTouchTracked = false;
+					}
+				}
+			}
+		}
+
+		private Vector2 AdjustAimDrag(Vector2 drag)
+		{
+			return drag * _horizontalAimSensitivity / 100.0f;
+		}
+
+		private float CalculateHorisontalRotationFromAimDrag(Vector2 drag)
+		{
+			return drag.x;
+		}
+	}
 }
