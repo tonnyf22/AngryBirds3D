72a378ac4fe7bd707a5f53c67ac6746e8403f427
Dmytro Buriak <buriak.dmytro1121@vu.cdu.edu.ua>
2024-10-24 16:13:03 +0300

    refactor(remove unused namespace)

--- Assets/Scripts/Throwables/Birds/Abilities/NoAbility.cs
+++ Assets/Scripts/Throwables/Birds/Abilities/NoAbility.cs
@@ -1,10 +1,8 @@
-using UnityEngine;
-
 namespace AngryBirds3D.Birds.Abilities
 {
     public class NoAbility : BirdAbility
     {
-        protected override void AbilityActivated()
+        protected override void AbilityActivatedByInput()
         {
             // specific ability logic
             // but this ability doing nothing, so just skip here
