ec38a26254bb7cc3826556a5426d66de39ddc43d
Dmytro Buriak <buriak.dmytro1121@vu.cdu.edu.ua>
2024-10-24 16:08:12 +0300

    feat(results sfx vfx): add effects to the completion state

--- Assets/Scripts/Level/States/CompletionState.cs
+++ Assets/Scripts/Level/States/CompletionState.cs
@@ -1,4 +1,6 @@
+using AngryBirds3D.Level.Audio;
 using AngryBirds3D.Level.Score;
+using AngryBirds3D.Level.Visuals;
 using AngryBirds3D.UserInterface.Level;
 using UnityEngine;
 
@@ -7,6 +9,11 @@ namespace AngryBirds3D.Level.States
     public class CompletionState : MonoBehaviour
     {
         [SerializeField]
+        private AudioEffects _audioEffects;
+        [SerializeField]
+        private VisualEffects _visualEffects;
+
+        [SerializeField]
         private ScoreSystem _scoreSystem;
 
         [SerializeField]
@@ -14,10 +21,19 @@ namespace AngryBirds3D.Level.States
 
         void OnEnable()
         {
+            _scoreSystem.CalculateCurrentSessionLevelScoreAndSaveBest();
+
             // vfx salute
             // sfx and / or music
-
-            _scoreSystem.CalculateCurrentSessionLevelScoreAndSaveBest();
+            if (_levelResults.IsSuccessfullyPassed())
+            {
+                _audioEffects.PlaySuccessfulPassClip();
+                _visualEffects.PlaySuccessfulPassEffect();
+            }
+            else
+            {
+                _audioEffects.PlayNotPassClip();
+            }
 
             // LevelScore ls = _scoreSystem.CurrentSessionLevelScore ;
 
