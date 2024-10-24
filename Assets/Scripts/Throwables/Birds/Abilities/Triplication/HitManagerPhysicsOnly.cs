93617a09b4d49536912fcac1492f214039132ae7
Dmytro Buriak <buriak.dmytro1121@vu.cdu.edu.ua>
2024-10-24 18:48:57 +0300

    feat(bird triplication physics only): add hit event

--- Assets/Scripts/Throwables/Birds/Abilities/Triplication/HitManagerPhysicsOnly.cs
+++ Assets/Scripts/Throwables/Birds/Abilities/Triplication/HitManagerPhysicsOnly.cs
@@ -1,3 +1,4 @@
+using System;
 using AngryBirds3D.Throwables;
 using UnityEngine;
 
@@ -5,11 +6,14 @@ namespace AngryBirds3D.Birds.Abilities.Triplication
 {
     public class HitManagerPhysicsOnly : MonoBehaviour
     {
+        public event Action OnHitOccuredEvent;
+
         [SerializeField]
         private ForwardLookManager _forwardLookManager;
 
         void OnCollisionEnter(Collision collision)
         {
+            OnHitOccuredEvent?.Invoke();
             _forwardLookManager.enabled = false;
         }
     }
