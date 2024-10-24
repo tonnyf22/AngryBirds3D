ff37b4bf4abb614ddf774d5e39bf99f398e08b19
Dmytro Buriak <buriak.dmytro1121@vu.cdu.edu.ua>
2024-10-24 19:22:51 +0300

    feat(audio): level sfx logic

---
+++ Assets/Scripts/Level/Audio/AudioEffects.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using AngryBirds3D.Audio;
+using UnityEngine;
+
+namespace AngryBirds3D.Level.Audio
+{
+    public class AudioEffects : ObjectAudio
+    {
+        [SerializeField]
+        private List<AudioClip> _startClips;
+        [SerializeField]
+        private List<AudioClip> _successfulPassClips;
+        [SerializeField]
+        private List<AudioClip> _notPassClips;
+
+        void Start()
+        {
+            AudioClip clip = GetRandomClip(_startClips);
+
+            _audioSource.PlayOneShot(clip);
+        }
+
+        public void PlaySuccessfulPassClip()
+        {
+            AudioClip clip = GetRandomClip(_successfulPassClips);
+
+            _audioSource.PlayOneShot(clip);
+        }
+
+        public void PlayNotPassClip()
+        {
+            AudioClip clip = GetRandomClip(_notPassClips);
+
+            _audioSource.PlayOneShot(clip);
+        }
+
+        void OnEnable()
+        {
+
+        }
+
+        void OnDisable()
+        {
+            
+        }
+    }
+}
