9903d884bdebd905fba17d110966ec8816e265c3
Dmytro Buriak <buriak.dmytro1121@vu.cdu.edu.ua>
2024-10-24 18:47:16 +0300

    feat(bird speed sfx): add event to inform about ability activation

--- Assets/Scripts/Throwables/Birds/Abilities/Speed.cs
+++ Assets/Scripts/Throwables/Birds/Abilities/Speed.cs
@@ -1,9 +1,12 @@
+using System;
 using UnityEngine;
 
 namespace AngryBirds3D.Birds.Abilities
 {
     public class Speed : BirdAbility
     {
+        public event Action AlibilityActivatedEvent;
+
         [SerializeField]
         [Range(1.0f, 100.0f)]
         private float _speedToAddImmediately;
@@ -15,11 +18,13 @@ namespace AngryBirds3D.Birds.Abilities
             _rb = GetComponent<Rigidbody>();
         }
 
-        protected override void AbilityActivated()
+        protected override void AbilityActivatedByInput()
         {
             _rb.AddForce(
                 _rb.mass * _speedToAddImmediately * transform.forward,
                 ForceMode.Impulse);
+
+            AlibilityActivatedEvent?.Invoke();
         }
     }
 }
