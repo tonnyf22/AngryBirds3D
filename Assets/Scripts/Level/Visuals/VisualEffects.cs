37c6c4d4e6deff4c8bb1eabd1b8e1d784837c186
Dmytro Buriak <buriak.dmytro1121@vu.cdu.edu.ua>
2024-10-24 19:29:19 +0300

    feat(audio): level vfx logic

---
+++ Assets/Scripts/Level/Visuals/VisualEffects.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using AngryBirds3D.Visuals;
+
+namespace AngryBirds3D.Level.Visuals
+{
+    public class VisualEffects : ObjectVisuals
+    {
+        [SerializeField]
+        private ParticleSystem _successfulPassParticleSystem;
+
+        public void PlaySuccessfulPassEffect()
+        {
+            InstantiateAndPlayVisualEffect(_successfulPassParticleSystem);
+        }
+
+        void OnEnable()
+        {
+
+        }
+
+        void OnDisable()
+        {
+            
+        }
+    }
+}
