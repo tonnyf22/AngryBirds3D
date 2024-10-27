3c8cc4030635de33ba3e069d0f2c59bbf8ff26be
Dmytro Buriak <buriak.dmytro1121@vu.cdu.edu.ua>
2024-10-27 23:40:37 +0200

    feat(egg throwable bird): add core logic for new bird type - black

---
+++ Assets/Scripts/Throwables/Birds/Abilities/Explosion/Explosion.cs
@@ -0,0 +1,35 @@
+using AngryBirds3D.Birds;
+using UnityEngine;
+
+namespace AngryBirds3D.Throwables.Birds.Abilities.Explosion
+{
+    public class Explosion : BirdAbility
+    {
+        [SerializeField]
+        private BirdLifeEndManager _birdLifeEndManager;
+
+        [SerializeField]
+        private GameObject _impactPrefab;
+
+        protected override void AbilityActivatedByInput()
+        {
+            Instantiate(_impactPrefab, transform.position, Quaternion.identity);
+
+            _birdLifeEndManager.NotifyAboutBirdLifeEnd();
+
+            // Destroy(gameObject);
+            // gameObject.SetActive(false);
+            MockBirdDestroyment();
+        }
+
+        private void MockBirdDestroyment()
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<SphereCollider>().enabled = false;
+
+            transform.GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(1).gameObject.SetActive(false);
+        }
+    }
+}
