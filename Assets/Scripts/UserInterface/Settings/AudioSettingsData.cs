5a626548cb93827df1e4e94ddf24c0664376f2c4
Dmytro Buriak <buriak.dmytro1121@vu.cdu.edu.ua>
2024-10-28 23:38:38 +0200

    feat(audio settings): create data container class

---
+++ Assets/Scripts/UserInterface/Settings/AudioSettingsData.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AngryBirds3D.UserInterface.Settings
+{
+    [Serializable]
+    public class AudioSettingsData
+    {
+        public float VolumeSFX;
+
+        public float VolumeMusic;
+    }
+}
