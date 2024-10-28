3e9883c66838a79811a7bd628c7f124cb8d2f2be
Dmytro Buriak <buriak.dmytro1121@vu.cdu.edu.ua>
2024-10-28 23:26:21 +0200

    feat(level ui data): load data from memory using json serialization

--- Assets/Scripts/UserInterface/Menu/LevelInfo.cs
+++ Assets/Scripts/UserInterface/Menu/LevelInfo.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using TMPro;
 using AngryBirds3D.Level.Score;
 using UnityEngine.SceneManagement;
-using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+using System.IO;
 
 namespace AngryBirds3D.UserInterface.Menu
 {
@@ -20,6 +20,112 @@ namespace AngryBirds3D.UserInterface.Menu
         [SerializeField]
         GameObject[] _starsObjects = new GameObject[3];
 
+        void Awake()
+        {
+            FillLevelScoreData();
+        }
+
+        private void FillLevelScoreData()
+        {
+            string filePath = CreateFilePathToJSON();
+
+            if (IsFileExists(filePath))
+            {
+                FillLevelScoreDataFromFile(filePath);
+            }
+            else
+            {
+                FillLevelScoreAsNew(filePath);
+            }
+        }
+
+        private string CreateFilePathToJSON()
+        {
+            return
+                Path.Combine(
+                    Application.persistentDataPath, 
+                    "LevelResult", 
+                    $"{_levelData.LevelNumber}.json");
+        }
+
+        private bool IsFileExists(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+
+        private void FillLevelScoreDataFromFile(string filePath)
+        {
+            string json = CreateJSONFromFile(filePath);
+            LevelScore levelScore = CreateLevelScoreFromJSON(json);
+            _levelData.LevelScore = levelScore;
+        }
+
+        private string CreateJSONFromFile(string filePath)
+        {
+            return File.ReadAllText(filePath);
+        }
+
+        private LevelScore CreateLevelScoreFromJSON(string json)
+        {
+            return JsonUtility.FromJson<LevelScore>(json);
+        }
+        
+        private void FillLevelScoreAsNew(string filePath)
+        {
+            Debug.Log("File does not exist: " + filePath);
+            Debug.Log("Creating file: " + filePath);
+
+            string directoryPath = CreateDirectoryPath(filePath);
+            if (IsDirectoryNotExists(directoryPath))
+            {
+                CreateDirectory(directoryPath);
+            }
+
+            LevelScore levelScore = CreateEmptyLevelScore();
+            _levelData.LevelScore = levelScore;
+
+            string json = CreateJSONFromLevelScore(levelScore);
+
+            WriteJSONToFile(json, filePath);
+        }
+        
+        private string CreateDirectoryPath(string filePath)
+        {
+            return Path.GetDirectoryName(filePath);
+        }
+
+        private bool IsDirectoryNotExists(string directoryPath)
+        {
+            return !Directory.Exists(directoryPath);
+        }
+
+        private void CreateDirectory(string directoryPath)
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+
+        private LevelScore CreateEmptyLevelScore()
+        {
+            return new LevelScore();
+        }
+
+        private string CreateJSONFromLevelScore(LevelScore levelScore)
+        {
+            return JsonUtility.ToJson(levelScore);
+        }
+
+        private void WriteJSONToFile(string json, string filePath)
+        {
+            try
+            {
+                File.WriteAllText(filePath, json);
+            }
+            catch (IOException e)
+            {
+                Debug.LogError("Failed to save JSON data to new file: " + e.Message);
+            }
+        }
+
         void Start()
         {
             _numberText.text = _levelData.LevelNumber;
@@ -31,27 +137,9 @@ namespace AngryBirds3D.UserInterface.Menu
             }
         }
 
-        void Update()
+        public void BeginLevel()
         {
-            ServeInput();
-        }
-
-        private void ServeInput()
-        {
-            if (Touch.activeTouches.Count > 0)
-            {
-                Touch firstTouch = Touch.activeTouches[0];
-
-                if (firstTouch.isTap)
-                {
-                    BeginLevel();
-                }
-            }
-        }
-
-        private void BeginLevel()
-        {
-            SceneManager.LoadScene(_levelData.LevelScene.BuildIndex);
+            SceneManager.LoadScene(_levelData.LevelScene.Name);
         }
     }
 }
